396a57361c1d23bec88ca5ef2c3cc21190065f27
YourAverageMo <Yilmaz@arizona.edu>
2024-08-21 15:19:14 -0700

    add column references, update diagram, add simple diagram

--- README.md
+++ README.md
@@ -7,8 +7,14 @@
 A ready to use full stack grocery store web application I made from scratch. Uses HTML/CSS/Bootstrap as the frontend, Python as the backend, and sqlite for the database.
 
 # Database Design
-The database is using SQLite and also SQLite3 library in python. For viewing, managing, and editing the database it was mostly done within the free VSCode extension SQLite3 Editor. I highly HIGHLY recommend this extension. Link 👇🏽
+The database is using SQLite and also SQLite3 library in python. For viewing, managing, and editing the database it was mostly done within the free VSCode extension SQLite3 Editor. I highly HIGHLY recommend this extension. Link 👇🏽  
 https://marketplace.visualstudio.com/items?itemName=yy0931.vscode-sqlite3-editor
+
+Here is a simple overview on the data flow:
+
+![Database Design Simple](./assets/db_diagram_simple.png)
+
+Here is a more detailed explanation on the columns each table has as well as the references:
 
 ![Database Design](./assets/db_diagram.png)
 
--- db/queries.sql
+++ db/queries.sql
@@ -20,8 +20,87 @@ CREATE TABLE
     )
 CREATE TABLE
     "orders" (
-        `order_id` INTEGER PRIMARY KEY UNIQUE NOT NULL,
+        `order_id` INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE NOT NULL,
         `customer_name` TEXT NOT NULL,
         `total` REAL NOT NULL,
         `datetime` DATETIME NOT NULL
+    );
+
+--
+-- adding references (foriegn keys)
+--
+PRAGMA foreign_keys = OFF;
+
+BEGIN;
+
+CREATE TABLE
+    "products" (
+        `product_id` INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE NOT NULL,
+        `name` TEXT NOT NULL,
+        `uom_id` INTEGER NOT NULL REFERENCES `uom` (`uom_id`) ON UPDATE CASCADE,
+        `price_per_unit` REAL NOT NULL
     )
+INSERT INTO
+    `products_tmp` (`product_id`, `name`, `uom_id`, `price_per_unit`)
+SELECT
+    `product_id`,
+    `name`,
+    `uom_id`,
+    `price_per_unit`
+FROM
+    `products`;
+
+DROP TABLE `products`;
+
+ALTER TABLE `products_tmp`
+RENAME TO `products`;
+
+COMMIT;
+
+PRAGMA foreign_keys = ON;
+
+,
+() PRAGMA foreign_keys = OFF;
+
+--
+--
+--
+BEGIN;
+
+CREATE TABLE
+    `orders_details_tmp` (
+        `order_id` INTEGER PRIMARY KEY NOT NULL REFERENCES `orders` (`order_id`) ON UPDATE CASCADE,
+        `product_id` INTEGER NOT NULL REFERENCES `products` (`product_id`) ON UPDATE CASCADE,
+        `quantity` REAL NOT NULL,
+        `total_price` REAL NOT NULL
+    );
+
+INSERT INTO
+    `orders_details_tmp` (
+        `order_id`,
+        `product_id`,
+        `quantity`,
+        `total_price`
+    )
+SELECT
+    `order_id`,
+    `product_id`,
+    `quantity`,
+    `total_price`
+FROM
+    `orders_details`;
+
+DROP TABLE `orders_details`;
+
+ALTER TABLE `orders_details_tmp`
+RENAME TO `orders_details`;
+
+COMMIT;
+
+PRAGMA foreign_keys = ON;
+
+,
+()
+--
+--
+--
